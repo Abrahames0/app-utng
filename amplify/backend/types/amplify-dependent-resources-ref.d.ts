b3a38363de4228b65c4264f4893257a57dc114d1
Abrahames <abtaham0212@gmail.com>
2024-07-04 13:26:16 -0600

    primer commit con amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "backutng": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
